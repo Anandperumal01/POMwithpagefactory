8df53f6de648b9248236a6b53be1d21d627a455c
Anandperumal01 <anandperumalece@gmail.com>
2024-07-12 09:19:13 +0530

    second

---
+++ src/test/java/testcases/LoginUser.java
@@ -0,0 +1,16 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class LoginUser {
+	
+	
+	@Test
+	public void login() {
+		
+		
+		System.out.println("i am in login Test");
+		
+	}
+
+}
